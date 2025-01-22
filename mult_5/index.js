e46e5d28181215c6c338c4d88372862bbd0caefe
script045 <devscript045@gmail.com>
2025-01-22 10:35:45 +0600

    init

--- mult_5/index.js
+++ mult_5/index.js
@@ -1,15 +1,17 @@
 
 let result = 0
-
+console.log("5 Multiplication with for loop: ")
 for(i=0; i<10; i++ ) {
     result += 5
-    console.log(result) 
+    console.log( "5 x " + (i+1) +" = "+   result) 
 }
 
+
+console.log("5 Multiplication with Do -while loop: ")
 let do_result = 0
 i=0
 while (i < 10) {
     do_result += 5
-    console.log(do_result) 
+    console.log( "5 x " + (i+1) +" = "+   do_result) 
     i++;
   }
